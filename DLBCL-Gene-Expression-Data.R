c056a24ab81972b0e49ac16ca4bfef88974aa975
Swetha Suresh Kumar <66764385+sweth068@users.noreply.github.com>
2023-11-02 12:46:01 -0400

    DLBCL-Gene-Expression-Data.R

--- DLBCL-Gene-Expression-Data.R
+++ DLBCL-Gene-Expression-Data.R
@@ -1,39 +1,46 @@
-#Step 1: Data Retrieval
-#We begin by downloading the DLBCL dataset and its associated class labels. 
-#The dataset is loaded into R, and missing values are accounted for using the read.table function.
+# Step 1: Data Acquisition and Preprocessing
 
-#Assign the path to the eisen.txt file and read it into a data frame
-data <- read.table("eisen.txt", 
-                   header = TRUE, na.strings = "NA", blank.lines.skip = FALSE, row.names = 1)
+# Assign the path to the eisen.txt file and read it into a data frame
+data <- read.table("eisen.txt", header = TRUE, na.strings = "NA", blank.lines.skip = FALSE, row.names = 1)
 
-#Step 2: Data Preprocessing
-#We process the dataset by reordering the columns to match the class labels. 
-#This allows us to separate data related to different DLBCL classes for further analysis.
+# Assign the path to eisenClasses.txt and read it into a data frame for class labels
+class_labels <- read.table("eisenClasses.txt", header = TRUE)
 
-#Subset the data frame with class labels
+# Check the first few rows of the data frame and class_labels data frame
+head(data)
+head(class_labels)
+
+
+# Step 2: Data Subset and Exploration
+
+# Subset the data frame with class labels
 cl <- as.character(class_labels[, 2])  
 data <- data[, cl]
 
+# Rearrange the columns
+dimnames(data)[[2]]
 
-#Step 3: Data Exploration
-#To better understand the data, we select a gene of interest, remove any missing values, 
-#and visualize the gene expression differences between two DLBCL classes using boxplots and histograms. 
-#This helps us identify the gene's characteristics within the dataset.
+# Split up classes and identify the gene of interest 
+GC_group <- 1:19 
+ACT_group <- 20:39
 
-#Splitting up the classes and look for the gene of interest
+# Splitting up the classes and look for the gene of interest
 x <- as.numeric(data[8000, GC_group])
 y <- as.numeric(data[8000, ACT_group])
 
-#Remove all NA's
+# Remove all NA's
 x <- x[!is.na(x)]; y <- y[!is.na(y)]
 
-#Plotting a boxplot
+
+# Step 3: Data Visualization
+
+# Plot a boxplot
 boxplot(list(GC_group = x, ACT_group = y), col = c("red", "blue"), 
         main = 'Example gene from DLBCL cDNA 2-channel dataset', 
         ylab = "log2(ratio intensity)",
         names = c("GC_group", "ACT_group"))
 
-#Plot individual histograms for both x and y
+# Plot individual histograms for both x and y
 hist(x, xlab = 'Selected gene', ylab = 'GC group', 
      main = "Gene selected from DLBCL cDNA 2-channel dataset", 
      col = c("red"))
@@ -42,33 +49,33 @@ hist(y, xlab = 'Selected gene', ylab = 'ACT group',
      main = "Gene selected from DLBCL cDNA 2-channel dataset", 
      col = c("blue"))
 
-#Step 4: Power and Sample Size Calculation - Single Gene
-#We calculate the pooled variance for the selected gene, which is a crucial parameter for power and sample 
-#size calculations. We then compute the minimum sample size needed to detect a 1.5-fold difference with 80% 
-#power and 99% confidence. This is accomplished using the pwr library for power analysis.
 
-#Calculate pooled variance
+# Step 4: Pooled Variance and Sample Size Calculation
+
+# Calculate pooled variance
 nx <- length(x)
 ny <- length(y)
 pool.var <- (((nx - 1) * var(x)) + ((ny - 1) * var(y))) / (nx + ny - 2)
 
+# Step 5: Sample Size Calculation
 
-#Step 5: Power and Sample Size Calculation - Multivariate Analysis
-#We calculate the sample size required to detect a difference empirically for the 
-#same gene using the determined delta between the two classes, assuming 99% confidence and 80% power. This further 
-#ensures that our analysis is robust and generalizable.
-
-#Load the pwr library for power analysis
+# Load the pwr library for power analysis
 library(pwr)
 
-#Calculate minimum sample size necessary to detect a 1.5-fold difference for the dataset
+# Calculate minimum sample size necessary to detect a 1.5-fold difference
 dif.1.5fold <- log2(1.5) / sqrt(pool.var)
 pl.ss3 <- pwr::pwr.t.test(d = dif.1.5fold, sig.level = 0.01, power = 0.8, type = "two.sample")
 
-#Calculate the difference (dif) based on your data
+# Print the pooled variance and minimum sample size
+cat("Pooled Variance:", pool.var, "\n")
+cat("Minimum Sample Size to Detect 1.5-Fold Difference:", pl.ss3$n, "\n")
+
+# Step 6: Sample Size Calculation with Empirical Data
+
+# Calculate the difference (dif) based on your data
 dif <- abs(mean(x) - mean(y)) / sqrt(pool.var)
 
-#Define your desired significance level, power, and type of test
+# Define your desired significance level, power, and type of test
 sig.level <- 0.01
 power <- 0.8
 test_type <- "two.sample"
@@ -76,13 +83,56 @@ test_type <- "two.sample"
 # Calculate the sample size for the new dataset
 pl.ss31 <- pwr::pwr.t.test(d = dif, sig.level = sig.level, power = power, type = test_type)
 
-#Step 6: Standard Deviation Calculation
-#To prepare for a proportion of genes vs. sample size analysis, we calculate the standard deviation 
-#for each gene in the dataset, considering missing values. We use the ssize and gdata libraries 
-#for this purpose.
+# Print the calculated difference and sample size
+cat("Difference (dif):", dif, "\n")
+cat("Sample Size (pl.ss31$n):", pl.ss31$n, "\n")
 
-#Calculate standard deviation for each gene in new_data
+
+# Step 7: Standard Deviation and Sample Size Calculation
+
+# Load the ssize library for sample size calculations
+library(ssize)
+
+# Load the gdata library for data manipulation
+library(gdata)
+
+# Read the data from eisen.txt again
+data_new <- read.table("C:/Users/sweth/OneDrive/Documents/R learning/Lab3/eisen.txt", 
+          header = TRUE, na.strings = "NA", blank.lines.skip = FALSE, row.names = 1)
+
+# Calculate standard deviation for each gene in the matrix
+sd_df <- apply(data_new, 1, function(row) sd(row, na.rm = TRUE))
+
+# Plot a histogram of the standard deviations
+hist(sd_df, n = length(sd_df), col = "red", border = "pink", main = "", xlab = "Standard Deviation (for data on the log2 scale)")
+
+
+# Step 8: Sample Size vs. Proportion of Genes
+
+fold.change <- 3.0
+power <- 0.8
+sig.level <- 0.05
+
+# Calculate standard deviation for each gene in new_data
 exp.sd <- apply(data_new, 1, sd, na.rm = TRUE)
 
-#Plot histogram of the variable
-hist(exp.sd, n = length(exp.sd), col = "red", border = "pink", main = "", xlab = "
+# Calculate sample size based on the new_data
+all.size <- ssize(sd = exp.sd, delta = log2(fold.change), sig.level = sig.level, power = power)
+
+# Plot the proportion of genes vs. sample size graph
+ssize.plot(all.size, lwd = 2, col = "blue", xlim = c(1, 20))
+xmax <- par("usr")[2] - 1
+ymin <- par("usr")[3] + 0.05
+
+# Add a legend to the graph
+legend(
+  x = xmax,
+  y = ymin,
+  legend = strsplit(paste("fold change =", fold.change, ",", "alpha =", sig.level, ",", "power =", power, ",", "# genes =", nrow(data_new), sep = ','), ",")[[1]],
+  xjust = 1,
+  yjust = 0,
+  cex = 1.0
+)
+
+# Set the title for the graph
+title("Sample Size to Detect Fold Change")
